5ffd5f56163e3b546af9745c220fe8cc47d83b06
Mihir Kumar <mihirpandey.13@gmail.com>
2019-07-14 12:40:01 +0530

    RoboFriends update 7 further modularization

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,86 +1,79 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import { setSearchField, requestRobots } from '../redux/actions';
+import { setSearchField, requestRobots } from "../redux/actions";
 
-import '../component-styles/MainComponentStyle.css';
+import "../component-styles/MainComponentStyle.css";
 
-import ErrorBoundary from './ErrorBoundaryComponent';
-import Search from './functional-components/SearchComponent';
-import Card from './functional-components/CardComponent';
-import Scroll from './functional-components/ScrollComponent';
+import ErrorBoundary from "./ErrorBoundaryComponent";
+import Search from "./functional-components/SearchComponent";
+import Card from "./functional-components/CardComponent";
+import Scroll from "./functional-components/ScrollComponent";
+import Header from "./functional-components/HeaderComponent";
 
 const mapStateToProps = state => {
-    return {
-        search: state.searchARobot.search,
-        robots: state.requestRobotsReducer.robots,
-        isPending: state.requestRobotsReducer.isPending,
-        error: state.requestRobotsReducer.error
-    };
+  return {
+    search: state.searchARobot.search,
+    robots: state.requestRobotsReducer.robots,
+    isPending: state.requestRobotsReducer.isPending,
+    error: state.requestRobotsReducer.error
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-        onRequestRobots: () => dispatch(requestRobots())
-    };
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  };
 };
 
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Welcome to RoboFriends",
+      loading: "Waiting for all the cool robots to come here...."
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            title: "Welcome to RoboFriends",
-            loading: "Waiting for all the cool robots to come here...."
-        };
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
+
+
+  render() {
+    const { loading, title } = this.state;
+
+    const { robots, search, onSearchChange, isPending } = this.props;
+
+    const filterRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(search.toLowerCase());
+    });
+
+    if (isPending) {
+      return (
+        <div className="pa2">
+          <Header isPending={isPending} title={title} />
+          <h1 className="tc f3 mt6 pa4">{loading}</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className="pa2">
+          <ErrorBoundary>
+            <Header isPending={isPending} title={title} />
+            <Search searchChange={onSearchChange} />
+            <Scroll>
+              <Card robots={filterRobots} />
+            </Scroll>
+          </ErrorBoundary>
+        </div>
+      );
     }
-
-    componentDidMount() {
-        // fetch('https://jsonplaceholder.typicode.com/users')
-        //     .then(response => response.json())
-        //     .then(json => this.setState({ robots: json }));
-        this.props.onRequestRobots();
-    }
-
-    // onSearchChange = (event) => {
-    //     this.setState({search: event.target.value});
-    // }
-
-    render() {
-
-        // Making this.state constants so that we don't have to right this.state again and again
-        const { loading, title } = this.state;
-        // Now removing every this.state
-
-        const { robots, search, onSearchChange, isPending } = this.props;
-
-        const filterRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(search.toLowerCase());
-        });
-
-        if(isPending) {
-            return (
-                <div className="pa2">
-                    <h1 className="tc f-subheadline lh-copy">{title}</h1>
-                    <h1 className="tc f3 mt6 pa4">{loading}</h1>
-                </div>
-            )
-        } else {
-            return (
-                <div className="pa2">
-                    <h1 className="tc f2 lh-copy">{title}</h1>
-                    <ErrorBoundary>
-                        <Search searchChange={onSearchChange} />
-                        <Scroll>
-                            <Card robots={filterRobots} />
-                        </Scroll>
-                    </ErrorBoundary>
-                </div>
-            );
-        }
-    }
-
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Main);
