4558c4923ab88b42d3c95314c910fe475c685f7f
Mihir Kumar <mihirpandey.13@gmail.com>
2019-07-14 12:55:35 +0530

    Updates

---
+++ precache-manifest.f99ad213396280ef6ee528f421cdb02d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36385fc78739a22af4b680a472740c8",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "a7f59fcbe7d48656b83f",
+    "url": "/RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8f6d74935bd66d507d49",
+    "url": "/RoboFriends/static/css/main.2a4f6255.chunk.css"
+  },
+  {
+    "revision": "a7f59fcbe7d48656b83f",
+    "url": "/RoboFriends/static/js/2.31fde999.chunk.js"
+  },
+  {
+    "revision": "8f6d74935bd66d507d49",
+    "url": "/RoboFriends/static/js/main.c52e7f63.chunk.js"
+  },
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  }
+]);
